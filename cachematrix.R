6a000486d4c97b3b27aec578c18d0e2bc6418185
ramdas2015 <ramdas.narayanan@bankofamerica.com>
2015-02-10 10:52:16 -0500

    Added the solution for Assignment 2 - R Programming

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,71 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix : This function creates a special matrix object that
+## can cache its inverse
 
-## Write a short comment describing this function
+## CacheSolve: This function computes the inverse of special matrix returned by
+## makeCacheMatrix above
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(x = numeric()) { 
+  
+  # holds the cached value or NULL if nothing is cached 
+  # initially nothing is cached so set it to NULL 
+  cache <- NULL 
+  
+  # store a matrix 
+  setMatrix <- function(newValue) { 
+    x <<- newValue 
+    # since the matrix is assigned a new value, flush the cache 
+    cache <<- NULL 
+  } 
+  
+  
+  # returns the stored matrix 
+  getMatrix <- function() { 
+    x 
+  } 
+  
+  
+  # cache the given argument  
+  cacheInverse <- function(solve) { 
+    cache <<- solve 
+  } 
+  
+  
+  # get the cached value 
+  getInverse <- function() { 
+    cache 
+  } 
+  
+  # return a list. Each named element of the list is a function 
+  list(setMatrix = setMatrix, getMatrix = getMatrix, cacheInverse = cacheInverse, getInverse = getInverse) 
+} 
 
-}
 
 
-## Write a short comment describing this function
+# The following function calculates the inverse of a "special" matrix created with  
+# makeCacheMatrix 
+cacheSolve <- function(y, ...) { 
+  # get the cached value 
+  inv <- y$getInverse() 
+  
+  if(!is.null(inv)) { 
+    message("getting cached data") 
+    return(inv) 
+  } 
+  # otherwise get the matrix, caclulate the inverse and store it in 
+  # the cache 
+  data <- y$getMatrix() 
+  inv <- solve(data) 
+  y$cacheInverse(inv) 
+  
+  # return the inverse 
+  inv
+} 
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+
+#a <- makeCacheMatrix( matrix(c(1,2,12,13), nrow = 2, ncol = 2) );
+#summary(a);
+#a$getMatrix();
+#cacheSolve(a);
+#cacheSolve(a);
+
+
